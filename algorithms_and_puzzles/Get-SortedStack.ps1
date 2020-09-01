2efcc82e022efdec4c16dbb942cb1c2a2314649d
TheUhrMacher <theuhrmacher@protonmail.com>
2020-09-01 16:15:10 -0400

    Function to return a sorted integer stack

---
+++ algorithms_and_puzzles/Get-SortedStack.ps1
@@ -0,0 +1,49 @@
+### Clear Console
+Clear-Host
+### ### ### ###
+
+# Initalize original stack
+[Object]$stack_original = New-Object System.Collections.Stack
+$stack_original.Push(4)
+$stack_original.Push(2)
+$stack_original.Push(3)
+$stack_original.Push(1)
+
+# Display initial values
+Write-Host 'Current Stack Values' -ForegroundColor Green
+$stack_original
+
+function Get-SortedStack {
+    [CmdletBinding()]
+    param (
+        [Object]$stack_input
+    );
+    # Initialize secondary stack and temporary container for sorting
+    [Object]$stack_sorted = New-Object System.Collections.Stack;
+    [int]$temp_value = 0;
+
+    # Pop the fist value directly into new stack
+    $stack_sorted.Push($stack_input.Pop());
+    # Continue until the original stack is empty
+    while ($stack_input.Count -gt 0) {
+        # Consider the top value in the input stack
+        $temp_value = $stack_input.Pop();
+        # Run while the current temp value is greater than than the top value
+        while ($stack_sorted.Count -gt 0 -and $temp_value -gt $stack_sorted.Peek()) {
+            # Push the top value back to the input stack
+            $stack_input.Push($stack_sorted.Pop());
+        };
+        # Add the temp value to the top of the sorted stack
+        $stack_sorted.Push($temp_value);
+    };
+    # Return the stack
+    return $stack_sorted
+};
+
+Write-Host
+Write-Host 'Sorted Stack Values' -ForegroundColor Green
+Get-SortedStack -stack_input $stack_original
+
+### Footer Space
+Write-Host
+### ### ### ###
